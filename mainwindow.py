d5ae2d5d2beb2d82df1422f01084ad4783074eb0
vamshivarsa <40311570+vamshivarsa@users.noreply.github.com>
2020-03-08 18:06:12 +0530

    Update mainwindow.py

--- mainwindow.py
+++ mainwindow.py
@@ -1,29 +1,26 @@
 from tkinter import *
 
-class DataEntry():
-    def __init__(self,rootone):
-        self.rootone = rootone
 
-    def bill_window(self):
+def bill_window():
         import billtable
-
-    def employee_window(self):
+            
+def employee_window():
         import employeetable
-
-        
-
 
 
 
 root = Tk()
-a = DataEntry(root)
 root.title("Multiple windows")
-
 root.geometry("560x360+120+120")
 
-label1=Label(text="THIS IS MAIN WINDOW",fg="green")
-label1.grid(row=3,column=3)
+canvas = Canvas(root,width=560,height=360)
+canvas.pack()
 
+image1= PhotoImage(file="pydatabase.PNG")
+canvas.create_image(0,0,image=image1,anchor=NW)
+
+label1=Label(canvas,text="THIS IS MAIN WINDOW",fg="green")
+label1.place()
 
 #menu section
 mymenu = Menu(root)
@@ -32,15 +29,8 @@ submenu=Menu(mymenu)
 mymenu.add_cascade(label="tables",menu=submenu)
 submenu.add_command(label="demotable",command="")
 submenu.add_separator()
-submenu.add_command(label="employee",command=a.employee_window)
+submenu.add_command(label="employee",command=lambda :employee_window())
 submenu.add_separator()
-submenu.add_command(label="bill",command=a.bill_window)
-
-
-
-
-
-
-
+submenu.add_command(label="bill",command=lambda:bill_window())
 
 root.mainloop()
